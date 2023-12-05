e5b9307b68ada6b28873b0d5f2feff18f9e583f1
Wenger Theo <theowenger88@gmail.com>
2023-12-05 16:03:22 +0100

    add .env

--- connexion.php
+++ connexion.php
@@ -1,10 +1,26 @@
 <?php
+$env = file_get_contents('.env');
+$env = explode("\n", $env);
 
-$user = 'root';
-$pass = 'root';
-$host = 'localhost';
-$port = 8889;
-$dbname = 'the_artbox';
+$dotenv = [];
+
+foreach ($env as $line) {
+    $line = trim($line);
+
+    if ($line === '' || $line[0] === '#') {
+        continue;
+    }
+
+    list($key, $value) = explode('=', $line, 2);
+    $dotenv[$key] = trim($value);
+}
+
+// Utilisez les valeurs du tableau $dotenv
+$user = $dotenv['DB_USER'] ?? '';
+$pass = $dotenv['DB_PASSWORD'] ?? '';
+$host = $dotenv['DB_HOST'] ?? '';
+$port = $dotenv['DB_PORT'] ?? '';
+$dbname = $dotenv['DB_NAME'] ?? '';
 
 try {
     $dbh = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
